cd2d93ba838e6ee213017e277424b449479f23b4
unknown <omic.sanjin92@gmail.com>
2024-03-13 18:08:20 +0100

    Add JS examples

---
+++ 0-JS_na_web_stranici/primjeri/scripts.js
@@ -0,0 +1,4 @@
+console.log ("Opet Algebra")
+
+var div = docment.getElementById("primjer");
+console.log(div.textContent)
---
+++ JS-vjezba-2/script.js
@@ -0,0 +1,46 @@
+/* Shift + alt + a = višelinijski komentar */
+
+/* 
+Možemo definirati varijable pomoću const let i var
+ */
+
+/* Const način definiranja varijable se mora prilikom deklaracije domah i inicijalizirati i ne može se mijenjati vrijednost */
+const js = "JS je suuper";
+
+
+/* Let način definiranja varijable se može deklarirati, naknadno inicijalizirati i može mu se mijenjnati vrijednost */
+let js2;
+js2 = "JS je super";
+
+var varX = 5;
+var varx = 6;
+
+console.log(js); 
+console.log(js2, varX)  
+
+// Vrste vrijednosti varijabli - primitivni i objektni...
+
+// brojevi
+
+const dob = 20;
+
+// tekst / string (teksutalna sekvenca, uvijek se stavlja u navodnike, svejedno jednostruke ili dvostruke)
+
+const imePrezime = "Sanjin Omić";
+
+//Boolean može bit samo true ili false
+
+const polaznik = true;
+
+//undifined npr. varijabla kojoj nismo dodjelili vrijednost 
+
+let brojPristunih;
+
+console.log(dob,brojPristunih,imePrezime)
+console.log(
+    typeof dob
+    
+    
+    
+
+)
